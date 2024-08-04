d7cbd61abc6cc030f1bb081a8a4bcdb4c14c489f
Tishbian Meshach <117526272+tishbian-meshach@users.noreply.github.com>
2024-08-04 19:25:22 +0530

    Update exp-10.c

--- exp-10.c
+++ exp-10.c
@@ -1,40 +1,45 @@
 //THREADING & SYNCHRONIZATION APPLICATIONS
 
-#include<stdio.h>
-#include<string.h>
-#include<pthread.h>
-#include<stdlib.h>
-#include<unistd.h>
-pthread_t tid[2];
-void* doSomeThing(void *arg)
-{
- unsigned long i = 0;
- pthread_t id = pthread_self();
- if(pthread_equal(id,tid[0]))
- {
- printf("\n First thread processing\n");
- }
- else
- {
- printf("\n Second thread processing\n");
- }
- for(i=0; i<(0xFFFFFFFF);i++);
- return NULL;
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+pthread_t threadIDs[2];  // Array to hold thread IDs
+
+// Function to be executed by each thread
+void* threadFunction(void *arg) {
+    unsigned long i = 0;
+    pthread_t currentThreadID = pthread_self();
+
+    if (pthread_equal(currentThreadID, threadIDs[0])) {
+        printf("\nFirst thread processing\n");
+    } else {
+        printf("\nSecond thread processing\n");
+    }
+
+    for (i = 0; i < (0xFFFFFFFF); i++);  // Simulate some work with a delay
+    return NULL;
 }
-int main(void)
-{
- int i = 0;
-40 | P a g e
- int err;
- while(i < 2)
- {
- err = pthread_create(&(tid[i]), NULL, &doSomeThing, NULL);
- if (err != 0)
- printf("\ncan't create thread :[%s]", strerror(err));
- else
- printf("\n Thread created successfully\n");
- i++;
- }
- sleep(5);
- return 0;
+
+int main(void) {
+    int i = 0;
+    int threadCreationError;
+
+    // Create two threads
+    while (i < 2) {
+        threadCreationError = pthread_create(&(threadIDs[i]), NULL, &threadFunction, NULL);
+        if (threadCreationError != 0) {
+            printf("\nCan't create thread: [%s]", strerror(threadCreationError));
+        } else {
+            printf("\nThread created successfully\n");
+        }
+        i++;
+    }
+
+    // Give enough time for the threads to finish their work
+    sleep(5);
+
+    return 0;
 }
